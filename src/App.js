8e5a08b658c54eeff3e926bf5bc607b57282e5fc
aresous <aresous@gmail.com>
2022-04-28 01:20:12 -0400

    header cursor animation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,43 @@ import Home from './components/Home'
 import Nav from './components/Nav'
 import Post from './components/Post'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+let intervId
+let cursorOn = true
 function App() {
     return (
         <Router>
             <div className="root">
                 <Link to="/">
-                    <header>./blog</header>
+                    <header
+                        onMouseEnter={() => {
+                            if (!intervId) {
+                                intervId = setInterval(() => {
+                                    let header =
+                                        document.querySelector('header')
+                                    if (cursorOn) {
+                                        header.style.borderRight =
+                                            'solid 2px white'
+                                        cursorOn = !cursorOn
+                                        return
+                                    }
+                                    if (!cursorOn) {
+                                        header.style.borderRight =
+                                            'solid 2px rgba(0,0,0,0)'
+                                        cursorOn = !cursorOn
+                                    }
+                                }, 400)
+                            }
+                        }}
+                        onMouseLeave={() => {
+                            clearInterval(intervId)
+
+                            let header = document.querySelector('header')
+                            intervId = null
+                            header.style.borderRight = ''
+                        }}
+                    >
+                        <span>./blog</span>
+                    </header>
                 </Link>
                 {/* <Nav /> */}
 
